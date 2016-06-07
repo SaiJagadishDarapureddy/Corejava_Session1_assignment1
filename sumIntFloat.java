d7f716053b88c97592fe217abd234f74ae131543
SaiJagadishDarapureddy <sai.jagadish.d@gmail.com>
2016-06-07 17:16:54 +0530

    Sum of two numbers
    Declare two variabers one of inter type and other of float type. perform
    addition and print it in the integer type

---
+++ sumIntFloat.java
@@ -0,0 +1,14 @@
+
+public class sumIntFloat {
+
+	public static void main(String[] args) {
+		int a=10;
+		float b=10.34f;
+		int c=(int)(a+b);
+		System.out.print("The Sum of two numbers is: " +c );
+		
+		
+		
+	}
+
+}
